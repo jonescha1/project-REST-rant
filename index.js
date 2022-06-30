0d11fe48f039f5e0f29a3eed3f25dfe2213319ab
Chase Jones <chaseejones@icloud.com>
2022-06-30 10:43:49 -0400

    Added 404 page route

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
+// This is the 404 wildcard route. If the user types in a route that does not exist, it will return this.
+// Make sure this route is at the bottom or it will override the other routes.
+app.get("*", (req, res) => {
+  res.status(404).send("<h1>404 Page</h1>");
+});
+
 app.listen(process.env.PORT);
