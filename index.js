9fe77bc7adb58bf257a6fcb640278a02d55c9095
Chase Jones <chaseejones@icloud.com>
2022-07-03 12:30:17 -0400

    Created 404 page view

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 // This is the 404 wildcard route. If the user types in a route that does not exist, it will return this.
 // Make sure this route is at the bottom or it will override the other routes.
 app.get("*", (req, res) => {
-  res.status(404).send("<h1>404 Page</h1>");
+  res.status(404).render("error404");
 });
 
 app.listen(process.env.PORT);
