10903532eaab8d7fac961142a60b229c936cda90
Chase Jones <chaseejones@icloud.com>
2022-07-09 10:42:13 -0400

    Created styling and layout of home page

--- views/home.jsx
+++ views/home.jsx
@@ -4,11 +4,35 @@ const Def = require("./default");
 function home({ title }) {
   return (
     <Def title={title}>
-      <div id="banner">
-        <img src="./images/alex-haney-CAhjZmVk5H4-unsplash.jpg" alt="" />
-      </div>
-      <h1>Home Page</h1>
-      <main></main>
+      <main>
+        <div className="overlay">
+          <div className="container">
+            <div className="banner">
+              <h2>This will be in bold and big letters!</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. In iure
+                obcaecati perspiciatis ea tempora molestias, fuga pariatur ipsa
+                ut veniam.
+              </p>
+            </div>
+          </div>
+        </div>
+        <img
+          className="homepage-image"
+          src="./images/brooke-lark-4J059aGa5s4-unsplash.jpg"
+          alt="food plate background"
+        />
+        <h4 id="photo-credits">
+          Photo by{" "}
+          <a href="https://unsplash.com/@brookelark?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            Brooke Lark
+          </a>{" "}
+          on{" "}
+          <a href="https://unsplash.com/s/photos/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            Unsplash
+          </a>
+        </h4>
+      </main>
     </Def>
   );
 }
