73761178e168954e18bf04fbb14d0467c8a417da
Chase Jones <chaseejones@icloud.com>
2022-07-12 19:23:34 -0400

    added 404 page image

--- views/error404.jsx
+++ views/error404.jsx
@@ -4,9 +4,22 @@ const Def = require("./default");
 function error404() {
   return (
     <Def>
-      <main>
+      <main className="container-fluid">
         <h1>404: PAGE NOT FOUND</h1>
-        <p>Oops, sorry, we are unable to find this page!</p>
+        <h4>Oops, sorry, we are unable to find this page!</h4>
+        <div className="page-image">
+          <img src="./images/cookie-the-pom-gySMaocSdqs-unsplash.jpg" alt="" />
+          <div>
+            Photo by{" "}
+            <a href="https://unsplash.com/@cookiethepom?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+              Cookie the Pom
+            </a>{" "}
+            on{" "}
+            <a href="https://unsplash.com/s/photos/404?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+              Unsplash
+            </a>
+          </div>
+        </div>
       </main>
     </Def>
   );
