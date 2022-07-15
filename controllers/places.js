f303884214f71c4eb2cb614caf2a309cd0c6936e
Chase Jones <chaseejones@icloud.com>
2022-07-14 21:04:00 -0400

    added edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -5,30 +5,12 @@ router.get("/", (req, res) => {
   res.render("places/index", { places, title: "Restaurants" });
 });
 
+// Router to new form page
 router.get("/new", (req, res) => {
   res.render("places/new");
 });
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = "http://placekitten.com/400/400";
-  }
-  if (!req.body.city) {
-    req.body.city = "My City";
-  }
-  if (!req.body.state) {
-    req.body.state = "USA";
-  }
-  places.push(req.body);
-  res.redirect("/places");
-});
-
-router.get("/:id/edit", (req, res) => {
-  res.send("places/edit");
-});
-
+// Showpage router
 router.get("/:id", (req, res) => {
   let id = Number(req.params.id);
   if (isNaN(id)) {
@@ -40,6 +22,62 @@ router.get("/:id", (req, res) => {
   }
 });
 
+router.get("/:id/edit", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/edit", { place: places[id], id });
+  }
+});
+
+// New router
+router.post("/", (req, res) => {
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = "http://placekitten.com/400/400";
+  }
+  //Default city if one is not provided
+  if (!req.body.city) {
+    req.body.city = "My City";
+  }
+  // Default state if one is not submitted
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body);
+  res.redirect("/places");
+});
+
+// This is the update router
+router.put("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = "http://placekitten.com/400/400";
+    }
+    //Default city if one is not provided
+    if (!req.body.city) {
+      req.body.city = "A Town";
+    }
+    // Default state if one is not submitted
+    if (!req.body.state) {
+      req.body.state = "A State";
+    }
+    // Save the new data into places[id]
+    places[id] = req.body;
+    res.redirect(`/places/${id}`);
+  }
+});
+
+// This is the delete router
 router.delete("/:id", (req, res) => {
   let id = Number(req.params.id);
   if (isNaN(id)) {
