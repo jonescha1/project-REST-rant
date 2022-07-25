6ba07a98265d87980b421c98791c84c806931641
Chase Jones <chaseejones@icloud.com>
2022-07-24 22:22:19 -0400

    changed mongoose connect to string

--- models/index.js
+++ models/index.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb+srv://Jonescha1:Cej0517562@learningcluster.4x72h.mongodb.net/rest-rant",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 module.exports.Place = require("./places");
 module.exports.Comment = require("./comment");
